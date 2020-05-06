07f528e42bcdb266eef2c191e5838c1dcf9e77bf
ExiWexi <47428024+ExiWexi@users.noreply.github.com>
2020-05-06 21:21:54 +0200

    0.0.0.4
    fix

--- ServerStart.sh
+++ ServerStart.sh
@@ -2,7 +2,7 @@
 
 # Start the server.
 start_server() {
-    java -server -Xms1024M -Xmx16G -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dfml.readTimeout=180 -jar forge.jar nogui
+    java -server -Xms1024M -Xmx16G -jar forge.jar nogui
 }
 
 echo "Starting FreaksideCraft Server..."
